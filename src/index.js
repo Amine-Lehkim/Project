f3e1d232345ec33531333758bb2fe1b1d2915795
Amine lehkim <74158555+Amine-Lehkim@users.noreply.github.com>
2024-02-02 21:45:09 +0100

    this is the changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
@@ -15,29 +14,14 @@ import Test from './Comp/Comment'
 import Login from './login'
 import Register from './register'
 import './App.css'
-=======
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
->>>>>>> origin/main
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     {/* <App />    */}
     {/* <Test /> */}
     <Login />
     {/* <Register /> */}
   </React.StrictMode>
 );
-=======
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
->>>>>>> origin/main
 reportWebVitals();
