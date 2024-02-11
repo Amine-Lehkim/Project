3d1688ff058d1cd63f1f29a9170329af4d288856
Amine lehkim <74158555+Amine-Lehkim@users.noreply.github.com>
2024-02-11 12:20:03 +0100

    Change the game card to be clickable

--- src/Comp/Favorites.js
+++ src/Comp/Favorites.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Favorites = () => {
@@ -17,17 +18,19 @@ const Favorites = () => {
       <div className="row">
         {favorites.map((game) => (
           <div key={game.id} className="col-md-4 mb-4">
-            <div className="card">
-              <img
-                src={game.background_image}
-                className="card-img-top"
-                alt={game.name}
-                style={{ height: '200px', objectFit: 'cover', borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{game.name}</h5>
+            <Link to={`/game/${game.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <div className="card">
+                <img
+                  src={game.background_image}
+                  className="card-img-top"
+                  alt={game.name}
+                  style={{ height: '200px', objectFit: 'cover', borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{game.name}</h5>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
